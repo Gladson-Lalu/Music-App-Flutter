543f1f56d913f1d30911518cd4a9a45acb3469bd
Gladson-Lalu <gladsonlalu2001@gmail.com>
2022-05-24 02:02:41 +0530

    Fixed Library music not found issue in release apk

--- lib/screens/local_music/widgets/album_section.dart
+++ lib/screens/local_music/widgets/album_section.dart
@@ -20,57 +20,76 @@ class _AlbumSectionState extends State<AlbumSection> {
         future: FetchMusicFile.albums,
         builder: (_, AsyncSnapshot<List<Album>> snapshot) {
           if (snapshot.hasData) {
-            return Flex(direction: Axis.vertical, children: [
-              Expanded(
-                child: GridView.builder(
-                  itemCount: snapshot.data!.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: (MediaQuery.of(context).orientation ==
-                              Orientation.portrait)
-                          ? 2
-                          : 3,
-                      crossAxisSpacing: margin,
-                      mainAxisSpacing: margin),
-                  itemBuilder: (BuildContext context, int index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            PageTransition(
-                                alignment: Alignment.bottomLeft,
-                                child: AlbumSongs(
-                                  album: snapshot.data![index],
+            return Flex(
+                direction: Axis.vertical,
+                children: [
+                  Expanded(
+                    child: GridView.builder(
+                      itemCount: snapshot.data!.length,
+                      gridDelegate:
+                          SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: (MediaQuery
+                                              .of(context)
+                                          .orientation ==
+                                      Orientation.portrait)
+                                  ? 2
+                                  : 3,
+                              crossAxisSpacing: margin,
+                              mainAxisSpacing: margin),
+                      itemBuilder: (BuildContext context,
+                          int index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                PageTransition(
+                                    alignment: Alignment
+                                        .bottomLeft,
+                                    child: AlbumSongs(
+                                      album: snapshot
+                                          .data![index],
+                                    ),
+                                    type: PageTransitionType
+                                        .scale));
+                          },
+                          child: GridTile(
+                              child: ClipRRect(
+                                borderRadius:
+                                    BorderRadius.circular(
+                                        10),
+                                child: Image(
+                                  image: snapshot
+                                      .data![index]
+                                      .coverImageProvider,
+                                  height: _width,
+                                  width: _width,
+                                  fit: BoxFit.cover,
                                 ),
-                                type: PageTransitionType.scale));
+                              ),
+                              footer: Padding(
+                                padding: EdgeInsets.only(
+                                    bottom: 5, left: 12),
+                                child: Text(
+                                  snapshot
+                                      .data![index].title,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      shadows: [
+                                        Shadow(
+                                            blurRadius: 1,
+                                            offset:
+                                                Offset.zero)
+                                      ],
+                                      fontSize: 14,
+                                      fontWeight:
+                                          FontWeight.bold),
+                                ),
+                              )),
+                        );
                       },
-                      child: GridTile(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image(
-                              image: snapshot.data![index].coverImageProvider,
-                              height: _width,
-                              width: _width,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          footer: Padding(
-                            padding: EdgeInsets.only(bottom: 5, left: 12),
-                            child: Text(
-                              snapshot.data![index].title,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  shadows: [
-                                    Shadow(blurRadius: 1, offset: Offset.zero)
-                                  ],
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          )),
-                    );
-                  },
-                ),
-              ),
-            ]);
+                    ),
+                  ),
+                ]);
           } else {
             return Center(
               child: CircularProgressIndicator(
